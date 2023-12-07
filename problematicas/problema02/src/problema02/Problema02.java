880d3a18e6851fa6dd2a8f2c548af04618b6e356
astalberto <albertoherrera205@hotmail.com>
2023-12-07 16:51:07 -0500

    Problema 02

--- problematicas/problema02/src/problema02/Problema02.java
+++ problematicas/problema02/src/problema02/Problema02.java
@@ -12,9 +12,24 @@ public class Problema02 {
 
     /**
      * @param args the command line arguments
+     *
+     * Genere una solución en lenguaje java que dé como salida números en orden
+     * invertido del 25 al 1.
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = 30;
+        int b = 60;
+        int c = 90;
+        int d = 120;
+        String acumulador = "";
+        for (int i = 21; i >= 1; i--) {
+            acumulador = String.format("%s%s-%s-%s-%s\n", acumulador, a, b, c, d);
+            a--;
+            b -= 2;
+            c -= 3;
+            d -= 4;
+        }
+        System.out.println(acumulador);
     }
-    
+
 }
